bf40789519fd63637451bf6ec062b4965c6bede9
PM <PSR10219@students.ephec.be>
2025-11-19 18:57:47 +0100

    UI Tests + Student adding required

--- ModelsDLL/Models/UI_Student.cs
+++ ModelsDLL/Models/UI_Student.cs
@@ -12,12 +12,12 @@ namespace ModelsDLL.Models
         // use public properties for WinForms data-binding
         [DisplayName("PSR-")] public int id { get; set; }
 
-        [DisplayName("First Name")] public string fname { get; set; }
-        [DisplayName("Last Name")] public string lname { get; set; }
+        [DisplayName("First Name")] public required string fname { get; set; }
+        [DisplayName("Last Name")] public required string lname { get; set; }
         [DisplayName("E-Mail")] public string? email { get; set; }
         [DisplayName("TEL")] public string? phone { get; set; }
         [DisplayName("Inscription")] public DateTime confirmed { get; set; }
-        [DisplayName("Section")] public string section { get; set; }
+        [DisplayName("Section")] public required string section { get; set; }
 
         public event PropertyChangedEventHandler? PropertyChanged;
     }
